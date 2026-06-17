be0528efd4301da8d119443414c97f77d4789db1
HERO-ZA <isekai5437@gmail.com>
2026-06-17 22:43:40 +0630

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { cors: { origin: "*" } });
 
-pp.use(express.static('public'));
+app.use(express.static('public'));
 
 let players = {};
 
